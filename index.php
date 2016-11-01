76b63d189123f5d696a1ee0dead5ed5fe8aeedcd
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2016-11-01 16:59:56 +0700

    Updated index.php

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ function filesize_format($size, $sizes = array('Bytes', 'KB', 'MB', 'GB', 'TB', 
         <h1><?=$sitetitle?></h1>
         <p><?=$sitetitle?>, is php script that useful to generate list latest modified file index to download for Your visitor.<br>CSS already using bootstrap 3.3.7, support minimum PHP 4.0.3, 5 and 7.<br>Just put in the folder on Your server, it doesn't need any database connections.</p>
         <p>
-          <a class="btn btn-lg btn-success" href="../../components/#navbar" role="button">Download <?=$sitetitle?> ver.<?=$version?></a>
+          <a class="btn btn-lg btn-success" href="https://github.com/aalfiann/PHPFileIndex" role="button">Download <?=$sitetitle?> ver.<?=$version?></a>
         </p>
       </div>
 
